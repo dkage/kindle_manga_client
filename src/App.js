5782a9e8a62ae7e78973308656f0fd8d34dd6aa5
Danilo Cunha <danilokage@gmail.com>
2022-09-23 02:03:04 -0300

    Testing style methods for MUI (styled, sx, classNames)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {useEffect} from "react";
 import {useCookies} from "react-cookie";
 import logo from './logo.svg';
-import './App.css';
 import Footer from "./components/layout/Footer";
+import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import {AppBar, Box, Button, Toolbar, Typography, IconButton } from "@mui/material";
 import Home from "./components/pages/Home"
 
 
@@ -20,7 +19,11 @@ function App() {
 
 
     // Custom Theme
-    const theme = createTheme({});
+    const theme = createTheme({
+        // palette: {
+        //     mode: "dark"
+        // }
+    });
 
 
     return (
--- src/components/menu/TopBar.js
+++ src/components/menu/TopBar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
+import {AppBar} from '@mui/material';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -10,21 +10,19 @@ import useTheme from "@mui/material/styles/useTheme";
 import { styled } from '@mui/material/styles';
 
 
-
-
 export default function TopBar() {
     const theme = useTheme();
 
-    const NewAppBar = styled(AppBar)(
-        ({ theme }) =>
-            `box-shadow: none`
-    )
+    // const AppBar = styled(AppBarDefault)(
+    //     ({ theme }) => `
+    //         box-shadow: none;
+    //         `)
 
     return (
 
         <Box sx={{flexGrow: 1}}>
 
-            <NewAppBar color="inherit">
+            <AppBar color="inherit" sx={{boxShadow: 'none'}}>
                 <Toolbar>
                     <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{mr: 2}}>
                         <MenuIcon/>
@@ -32,10 +30,10 @@ export default function TopBar() {
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                         KindleManga
                     </Typography>
-                    {/*<Button color="inherit">Create Account</Button>*/}
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit" variant="outlined" sx={{mr:2}}>Create Account</Button>
+                    <Button color="inherit" variant="outlined">Login</Button>
                 </Toolbar>
-            </NewAppBar>
+            </AppBar>
 
         </Box>
 
